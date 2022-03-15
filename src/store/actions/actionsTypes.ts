1f10386c029a7ea78e56b8102798f518dabc34d9
Ilona <ilonakot92@gmail.com>
2022-03-15 12:32:56 +0300

    refactor: change action types name

--- src/store/actions/actionsTypes.ts
+++ src/store/actions/actionsTypes.ts
@@ -1,7 +1,6 @@
-export const GET_TODOS = "TODO/GET_TODO";
-export const ADD_TODO = "TODO/ADD_TODO";
-export const TOGGLE_TODO = "TODO/TOGGLE_TODO";
-export const REMOVE_TODO = "TODO/REMOVE_TODO";
-export const UPDATE_TODO = "TODO/UPDATE_TODO";
-
-export const IS_EDIT = "APP/ID_EDIT";
+export const GET_TODOS = "GET_TODO";
+export const ADD_TODO = "ADD_TODO";
+export const TOGGLE_TODO = "TOGGLE_TODO";
+export const REMOVE_TODO = "REMOVE_TODO";
+export const UPDATE_TODO = "UPDATE_TODO";
+export const IS_EDIT = "IS_EDIT";
